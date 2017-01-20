bfe8418a1b9948bb60ebf35a5ca57f3e7c08d53e
scott-wyatt <scottwyatt86@gmail.com>
2017-01-20 13:55:47 -0500

    bump proxy-generics

--- test/fixtures/FakeShipping.js
+++ test/fixtures/FakeShipping.js
@@ -4,4 +4,13 @@ module.exports = class FakeShippingProvider {
   constructor(options) {
     this.options = options
   }
+  validateAddress(data) {
+    return Promise.resolve(data)
+  }
+  getRate(data) {
+    return Promise.resolve({})
+  }
+  getRates(data) {
+    return Promise.resolve([])
+  }
 }
