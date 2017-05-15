edc10dde0ec935d82228c53efd6221688ba0f086
scott-wyatt <scottwyatt86@gmail.com>
2017-05-15 12:03:13 -0400

    Product Schema

---
+++ api/controllers/CouponController.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const Controller = require('trails/controller')
+
+/**
+ * @module CouponController
+ * @description Generated Trails.js Controller.
+ */
+module.exports = class CouponController extends Controller {
+  generalStats(req, res) {
+    res.json({})
+  }
+}
+
---
+++ api/controllers/DiscountController.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const Controller = require('trails/controller')
+
+/**
+ * @module DiscountController
+ * @description Generated Trails.js Controller.
+ */
+module.exports = class DiscountController extends Controller {
+  generalStats(req, res) {
+    res.json({})
+  }
+}
+
